8bdfb9dac3b4a770e74a6f4221edf9517a5f5661
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-30 17:43:24 -0500

    Create backdrop image view, movie tv search view to view folder

---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/TVShowBackdropView.swift
@@ -0,0 +1,20 @@
+//
+//  TVShowBackdropCard.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 5/30/25.
+//
+
+import SwiftUI
+
+struct TVShowBackdropView: View {
+    let tvShow: TVShow
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TVShowBackdropView()
+}
