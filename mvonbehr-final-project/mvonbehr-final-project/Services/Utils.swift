490a78374fdc69da4ba67d1d4faa0ed970cc698f
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-30 16:54:02 -0500

    Add documentation to Utils methods

--- mvonbehr-final-project/mvonbehr-final-project/Services/Utils.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Services/Utils.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 class Utils {
+    /// Pre-configured JSONDecoder optimized for TMDB API responses
     static let jsonDecoder: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .formatted(dateFormatter)
         return decoder
     }()
-    
+
+    /// DateFormatter configured for TMDB's standard date format
     static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
