c7d87fa65cc27af27a68d0d531f08794ceca3814
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-30 13:10:13 -0500

    Add TV Show Error to represent errors when making API call, add comments

--- mvonbehr-final-project/mvonbehr-final-project/Services/TVShowService.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Services/TVShowService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol TVShowService {
+    
+}
+
+enum TVShowEndpoint: String {
+    case search = "/search/tv"
+    case details = "/tv"
+}
+
+enum TVShowError: Error {
+    // General API failure when the request cannot be completed
+    case apiError
+    
+    // Error when the HTTP response status code is not in the range between 200 and 300
+    case invalidResponse
+    
+    // Error when constructing the endpoint URL
+    case invalidEndpoint
+    
+    // Error when server returned empty response or missing expected data
+    case noData
+    
+    // JSON Decoding error
+    case serializationError
+}
