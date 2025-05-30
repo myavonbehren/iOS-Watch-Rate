e03300b2cee49244c6eb2d596813ebd81386bfeb
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-30 10:24:17 -0500

    Create UI view for searching TV shows

---
+++ mvonbehr-final-project/mvonbehr-final-project/TVSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  TVSearchView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 5/30/25.
+//
+
+import SwiftUI
+
+struct TVSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TVSearchView()
+}
