5aa2df040dc592d0180f312dc7eb2680f54eb261
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 15:17:13 -0500

    Create tv show detail state

---
+++ mvonbehr-final-project/mvonbehr-final-project/TVShowDetailState.swift
@@ -0,0 +1,8 @@
+//
+//  TVShowDetailState.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import Foundation
---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/ReviewDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewDetailView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+ 
+import SwiftUI
+
+struct ReviewDetailView: View {
+    let showId: Int
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewDetailView(showId: TVShow.mockTVShow.id )
+}
