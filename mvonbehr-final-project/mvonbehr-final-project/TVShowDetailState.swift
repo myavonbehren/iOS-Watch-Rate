89ed0ed2181013ac95e55bdb1cb8098025474593
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 15:32:38 -0500

    Create tv show detail state for ui

--- mvonbehr-final-project/mvonbehr-final-project/TVShowDetailState.swift
+++ mvonbehr-final-project/mvonbehr-final-project/TVShowDetailState.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class TVShowDetailState: ObservableObject {
+    private let tvShowService: TVShowService
+    @Published var tvShow: TVShow?
+    @Published var isLoading: Bool = false
+    @Published var error: NSError?
+    
+    init(tvShowService: TVShowService = TVShowStore.shared) {
+        self.tvShowService = tvShowService
+    }
+    
+    func loadTVShow(id: Int) async {
+        if Task.isCancelled { return }
+        
+        await MainActor.run {
+            isLoading = true
+            error = nil
+            tvShow = nil
+        }
+        
+        do {
+            let show = try await tvShowService.fetchTVShow(id: id)
+            await MainActor.run {
+                tvShow = show
+                isLoading = false
+            }
+        } catch {
+            await MainActor.run {
+                self.error = error as NSError
+                self.isLoading = false
+            }
+        }
+        
+    }
+}
