68ac7dc4863d7d854c5258c34c2861e26e49abc3
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 18:32:05 -0500

    Create empty place holder view

---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/EmptyPlaceHolderView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyPlaceHolderView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import SwiftUI
+
+struct EmptyPlaceHolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyPlaceHolderView()
+}
