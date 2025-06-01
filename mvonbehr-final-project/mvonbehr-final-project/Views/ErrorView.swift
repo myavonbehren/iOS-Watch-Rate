6f7dfe3492da1cfa172c03797ca6a8693d59294e
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 15:55:28 -0500

    Create error view

---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
