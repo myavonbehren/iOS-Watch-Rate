49eb5e0ebe828d7e215b2ab7e8c1420230f08aba
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 17:17:02 -0500

    Create data fetch phase to track loading, success, failture and empty states

--- mvonbehr-final-project/mvonbehr-final-project/Bindings/DataFetchPhase.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Bindings/DataFetchPhase.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum DataFetchPhase<V> {
+    case empty
+    case success(V)
+    case failure(Error)
+    
+    var value: V? {
+        if case .success(let value) = self {
+            return value
+        }
+        return nil
+    }
+}
--- mvonbehr-final-project/mvonbehr-final-project/Bindings/TVShowSearchState.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Bindings/TVShowSearchState.swift
@@ -11,5 +11,24 @@ import Foundation
 
 class TVShowSearchState: ObservableObject {
     @Published var query = ""
-    @Published private(set) var phase:
+    @Published private(set) var phase: DataFetchPhase<[TVShow]> = .empty
+    
+    private var cancellables = Set<AnyCancellable>()
+    private let tvShowService: TVShowService
+    
+    private var trimmedQuery: String {
+        query.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    var tvShows: [TVShow] {
+        phase.value ?? []
+    }
+    
+    init(tvShowService: TVShowService = TVShowStore.shared) {
+        self.tvShowService = tvShowService
+    }
+    
+    func startObserve() {
+    }
+    
 }
