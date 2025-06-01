eb17b82532d470b7c622b1eb40459194582c93b6
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 18:20:55 -0500

    Link review form view, add a row view

--- mvonbehr-final-project/mvonbehr-final-project/Views/TVSearchView.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Views/TVSearchView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct TVSearchView: View {
+    @StateObject var tvSearchState = TVShowSearchState()
+    
     var body: some View {
-        NavigationView {
-            List {
-                Text("Hello")
+        List {
+            ForEach(tvSearchState.tvShows) { tvShow in
+                NavigationLink(destination: ReviewFormView(showId: tvShow.id)){
+                    
+                }
             }
         }
+        .onAppear{
+            self.tvSearchState.startObserve()
+        }
+        .navigationTitle(Text("Search TV Shows"))
+    }
+    
+}
+
+struct TVShowRowView: View {
+    let tvShow: TVShow
+    
+    var body: some View {
+        
     }
 }
 
