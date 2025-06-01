cd3284985a0f44fe64a9a27c4fa2a2bf842698ca
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 17:06:39 -0500

    Create data fetch phase

--- mvonbehr-final-project/mvonbehr-final-project/Bindings/TVShowSearchState.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Bindings/TVShowSearchState.swift
@@ -5,4 +5,11 @@
 //  Created by Mya Von Behren on 6/1/25.
 //
 
+import SwiftUI
+import Combine
 import Foundation
+
+class TVShowSearchState: ObservableObject {
+    @Published var query = ""
+    @Published private(set) var phase:
+}
