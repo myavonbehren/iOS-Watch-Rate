37bef46460c04d5c8221155da2d79e6e61c5d22d
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 15:08:25 -0500

    Refactor backdrop view placeholder to include show name

--- mvonbehr-final-project/mvonbehr-final-project/Views/TVShowBackdropView.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Views/TVShowBackdropView.swift
@@ -20,13 +20,21 @@ struct TVShowBackdropView: View {
                 AsyncImage(url: show.backdropURL) { image in
                     image.resizable()
                 } placeholder: {
-                        Rectangle()
-                            .fill(Color.black.opacity(0.5))
-                            .overlay(
+                    Rectangle()
+                        .fill(Color.black.opacity(0.5))
+                        .overlay(
+                            VStack {
                                 Image(systemName: "photo")
                                     .foregroundColor(.white)
                                     .font(.largeTitle)
-                            )
+                                Text(show.name)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                            }
+                        )
+                        .cornerRadius(8)
                 }
             }
             .aspectRatio(16/9, contentMode: .fit)
