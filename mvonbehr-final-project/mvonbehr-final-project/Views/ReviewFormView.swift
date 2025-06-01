ad8592a37e947576b892235272321f387fd123ed
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 18:20:19 -0500

    Create review form view to enter reviews

---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/ReviewFormView.swift
@@ -0,0 +1,22 @@
+//
+//  ReviewFormView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import SwiftUI
+
+struct ReviewFormView: View {
+    let showId: Int
+    
+    var body: some View {
+        Form {
+            Text("")
+        }
+    }
+}
+
+#Preview {
+    ReviewFormView(showId: TVShow.mockTVShow.id)
+}
