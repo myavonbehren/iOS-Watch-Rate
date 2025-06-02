1f6efb99cbf0af3da5a911d6959bd776dec729c1
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 19:11:08 -0500

    Create loading view

--- mvonbehr-final-project/mvonbehr-final-project/Views/Overlay Views/LoadingView.swift
+++ mvonbehr-final-project/mvonbehr-final-project/Views/Overlay Views/LoadingView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (spacing: 30) {
+            ProgressView()
+                .scaleEffect(1.5)
+            Text("Loading TV show details...")
+                .foregroundColor(.secondary)
+        }
     }
 }
 
