714d6544e66edbfdf06c24547b1f4bdf90c68e80
Mya Von Behren <mvonbehr@depaul.edu>
2025-06-01 20:12:46 -0500

    Create review and watch list view

---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/ReviewListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewListView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import SwiftUI
+
+struct ReviewListView: View {
+    var body: some View {
+        Text("Reviews")
+    }
+}
+
+#Preview {
+    ReviewListView()
+}
---
+++ mvonbehr-final-project/mvonbehr-final-project/Views/WatchListView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchListView.swift
+//  mvonbehr-final-project
+//
+//  Created by Mya Von Behren on 6/1/25.
+//
+
+import SwiftUI
+
+struct WatchListView: View {
+    var body: some View {
+        Text("Watch List")
+    }
+}
+
+#Preview {
+    WatchListView()
+}
